0cf00e333fe280dc52aaa7b08ed010462852e858
James Kim <james@sidenotes.xyz>
2024-11-25 17:00:20 +0900

    Initial Commit

---
+++ DisneyQuiz/DisneyQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyQuizApp.swift
+//  DisneyQuiz
+//
+//  Created by James Kim on 11/25/24.
+//
+
+import SwiftUI
+
+@main
+struct DisneyQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
